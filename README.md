95f804e582e336f49744658dea10e8a45efb39a6
Rishavkumar16 <97583928+Rishavkumar16@users.noreply.github.com>
2024-05-01 21:40:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## By Akshun Verma
+
+### Project Name: Paradise Fitness Gym
+ I made this website with the help of Html, CSS, JS and React. This website is about the Paradise Fitness Gym which is located in Begumpur Delhi. 
+
+ 
+### ✍️ Author and 📞 Contact
+
+   - [![Instagram](https://img.shields.io/badge/Instagram-0A66C2?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/akshunn_3945/)
+   - [![LinkedIn](https://img.shields.io/badge/-LinkedIn-blue)](https://www.linkedin.com/in/akshun-verma-98110b214/)
+
+#### Here is the link of the website
+-[Click Here](https://paradise-fitness-gym.netlify.app/)
+
+- ![](https://img.shields.io/badge/HTML--CSS-JavaScript-lightgrey)
+
+
+ ### Feedback
+ - If you have any feedback, please reach out to me at akshunverma45@gmail.com
---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useRef } from 'react'
+import './Join.css'
+import emailjs, { send } from '@emailjs/browser'
+const Join = () => {
+
+    const form=useRef(); 
+    const sendEmail = (e) => {
+        e.preventDefault(); 
+        emailjs.sendForm('service_f1v3pci', 'template_k6ndqvd', form.current, 'JrcAPV8pj_DtbsTPr')
+          .then((result) => {
+          
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+    
+
+  return (
+    <div className="Join" id='join-us'>
+        <div className="left-j">
+            <hr />
+            <div>
+                <span  className='stroke-text'>READY TO</span>
+                <span>LEVEL UP</span>
+            </div>
+
+            <div>
+                <span>YOUR BODY</span>
+                <span className='stroke-text'>WITH US?</span>
+            </div>
+        </div>
+
+
+        {/* right side  */}
+        <div className="right-j">
+            <form ref={form} className="email-container" onSubmit={sendEmail}>
+<input type="email" name='user_email' placeholder='Enter your email' />
+  <button className='btn btn-j'>Join Now</button>
+            </form>
+        </div>
+
+    </div> 
+  )
+}
+
+export default Join
